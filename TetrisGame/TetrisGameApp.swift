484647427a91c212f29ae638d01077a3bfcd12bc
josty11 <josty11@mail.ru>
2025-05-25 21:08:02 +0200

    Initial Commit

---
+++ TetrisGame/TetrisGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisGameApp.swift
+//  TetrisGame
+//
+//  Created by Татьяна on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TetrisGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
